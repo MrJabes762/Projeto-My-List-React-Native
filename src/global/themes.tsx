3720bf592da41c9432b16b13751ad0b03293b38c
Mr Jabes <jabessilva31@gmail.com>
2024-10-23 23:09:36 -0300

    Atualização:
    * Criação da Paleta de Cores

---
+++ src/global/themes.tsx
@@ -0,0 +1,9 @@
+export const themes = {
+    colors:{
+        vermelhoIfba: '#C90C0E',
+        verdeCLaroIfba: '#79BA43',
+        verdeMaisouMenos: '#31A042',
+        verdeEscuroIfba: '#0B671B',
+        black:'black'
+    }
+}
