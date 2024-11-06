1ca86a4c45a092ad66c7667dd2b355d9bad74ad3
Jabes da Silva Cajazeira <118413268+MrJabes762@users.noreply.github.com>
2024-11-06 18:10:59 -0300

    Desenvolvimento: Logica inicial para exibição da segunda tela na barra de navegação ao pressionar o botão de adição  (#3)
    * Atuaizações: Ajuste de Dependencias
    
    * Atualizações:
    
    *Criação do Auth Context para logica inicial de exibição do modal de adição de tarefas

--- src/routes/botao.routes.tsx
+++ src/routes/botao.routes.tsx
@@ -2,26 +2,29 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import User from '../pages/user';
 import List from '../pages/list';
 import CustomTabBar from '../components/CustomTabBar';
+import { AuthProviderList } from '../context/authContext_list';
 
 const Tab = createBottomTabNavigator();
 
 export default function Botoes() {
-  return (
-    <Tab.Navigator
-      screenOptions={{
-        headerShown: false // sem a barra do cabeçalho
-      }}
-      tabBar={pros => <CustomTabBar {...pros}
-      />}
-    >
-      <Tab.Screen
-        name="List"
-        component={List}
-      />
-      <Tab.Screen
-        name="User"
-        component={User}
-      />
-    </Tab.Navigator>
+  return (// o AuthProvider engloba todas as telas explicidas 
+    <AuthProviderList>
+      <Tab.Navigator
+        screenOptions={{
+          headerShown: false // sem a barra do cabeçalho
+        }}
+        tabBar={pros => <CustomTabBar {...pros}
+        />}
+      >
+        <Tab.Screen
+          name="List"
+          component={List}
+        />
+        <Tab.Screen
+          name="User"
+          component={User}
+        />
+      </Tab.Navigator>
+    </AuthProviderList>
   );
 }
