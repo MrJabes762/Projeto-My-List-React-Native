a0ad432929fc22ee155d55a32a84e9868c4e8bf7
Mr Jabes <jabessilva31@gmail.com>
2024-11-13 00:37:15 -0300

    Atualizações:
    *Criação do icone de seleção do card

---
+++ src/components/Selecionar/style.ts
@@ -0,0 +1,11 @@
+import { Dimensions, StyleSheet } from "react-native";
+import { themes } from "../../global/themes";
+
+export const styleSelecionar = StyleSheet.create ({
+    circulo:{
+        width:20,
+        height:20,
+        borderRadius:10,
+        borderWidth:1
+    }
+});
