e56e87ba5b999ba5662b0aeaf411ba3068df44ed
Mr Jabes <jabessilva31@gmail.com>
2024-11-13 00:36:23 -0300

    Atuaizações:
    *Construção inicial da tela de listagem

--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -1,13 +1,106 @@
 import React from "react";
-import { Text, View } from "react-native"
+import { FlatList, Text, TouchableOpacity, View } from "react-native"
 import { styleList } from "./style";
+import { Input } from "../../components/Input";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Selecionar } from "../../components/Selecionar";
+
+
+type PropCard = {
+    id:number,
+    titulo:string,
+    descricao:string,
+    prioridade:'Urgente' | 'Opcional'
+}
+const data:Array<PropCard> = [
+    {
+        id:0,
+        titulo: "Realizar tarefa de Dados",
+        descricao:"Buscar informações em uma API externa e mesclar os dados",
+        prioridade: "Opcional"
+    },
+    {
+        id:1,
+        titulo: "Construir diagrama de algoritmos ",
+        descricao:"Usar o Drawio para confecionar diagramas ",
+        prioridade: "Urgente"
+    },
+    {
+        id:2,
+        titulo: "Realizar Pull requests no código central",
+        descricao:"Avaliar código e mesclar com a branch principal",
+        prioridade: "Urgente"
+    },
+    {
+        id:3,
+        titulo: "Levantar Requisitos dos Softwares",
+        descricao:"Entrevista com os clientes para aferição das necessidades",
+        prioridade: "Urgente"
+    },
+    {
+        id:4,
+        titulo: "Ajuste de Servidor",
+        descricao:"Verificar a logica de requisição das informações",
+        prioridade: "Opcional"
+    },
+    {
+        id:5,
+        titulo: "detalhamento da arquitetura",
+        descricao:"Levantar as infos e definir arquitetura",
+        prioridade: "Urgente"
+    }
+]
 
 export default function List() {
+    const card = (item:PropCard) =>{
+        return (
+            <TouchableOpacity style = {styleList.card}>
+                <View style= {styleList.rowCard}>
+                    <View style = {styleList.rowCardEsqueda}>
+                        <Selecionar />
+                            <View>
+                                <Text 
+                                    style = {styleList.cardTitulo} 
+                                    numberOfLines= {1} // exibir a info em uma unica linha 
+                                    ellipsizeMode = {'tail'}//para adicionar os 3 pontos 
+                                    >{item.titulo}
+                                </Text>
+                                <Text 
+                                    style = {styleList.cardDesc}
+                                    numberOfLines= {1} 
+                                    ellipsizeMode = {'tail'}
+                                    >{item.descricao}</Text>
+                            </View>
+                    </View>
+                    {/*<Prioridade/>*/}
+                </View>
+            </TouchableOpacity>
+        );
+                        
+    }
     return (
         <View style = {styleList.Tela}>
-            <Text>
-                Olá Mundo Lista
-            </Text>
+            <View style = {styleList.cabecalho}>
+                <Text style = {styleList.textoCabecalho}>
+                    Bom dia, Jabes
+                </Text>
+                <View style = {styleList.viewInput}>
+                    <Input
+                        IconLeft={MaterialIcons}
+                        IconLeftName="search"
+                    />
+                </View>
+            </View>
+            <FlatList
+                data={data}
+                style= {styleList.lista}
+                keyExtractor={(item,index) => item.id.toString()}
+                renderItem={({item,index}) => {
+                    return (
+                        card(item)
+                    );
+                }}
+            />        
         </View>
     );
-} 
+}
--- src/pages/list/style.ts
+++ src/pages/list/style.ts
@@ -1,11 +1,64 @@
-import { StyleSheet } from "react-native";
+import { Dimensions, StyleSheet } from "react-native";
 import { themes } from "../../global/themes";
 
 export const styleList = StyleSheet.create ({
     Tela:{
+        flex:1,
         width:'100%',
         height:'100%',
-        justifyContent:'center',
         alignItems: 'center',
+        backgroundColor:themes.colors.verdeCLaroIfba
+    },
+    cabecalho:{
+        width:'100%',
+        height:Dimensions.get("window").height/5,
+        backgroundColor:themes.colors.verdeEscuroIfba,
+        paddingHorizontal:20,
+        justifyContent:'center'
+    },
+    textoCabecalho:{
+        fontSize:18,
+        color: 'white',
+        marginTop: 20
+    },
+    viewInput:{
+        width:'80%'
+    },
+    lista:{
+        marginTop:40,
+        paddingHorizontal:10
+    },
+    card:{
+        width: '100%',
+        height:65,
+        backgroundColor:'white',
+        marginTop:10,
+        borderRadius:10,
+        padding:5,
+        justifyContent:'center',
+        borderColor:themes.colors.verdeEscuroIfba,
+        borderWidth:2
+    },
+    rowCard:{
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'space-between'
+    },
+    rowCardEsqueda:{
+        width:'70%',
+        flexDirection:'row',
+        alignItems:'center',
+        gap:10
+    },
+    cardTitulo:{
+        width:200,
+        fontWeight:'bold',
+        fontSize:14,
+        color:themes.colors.verdeEscuroIfba
+    },
+    cardDesc:{
+        fontStyle:'italic',
+        fontSize:14,
+        color:themes.colors.verdeCLaroIfba
     }
 });
