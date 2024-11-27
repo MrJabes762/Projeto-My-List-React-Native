77bffc7b4ef9d2ec9e037a81fa50242089ff0e0b
Mr Jabes <jabessilva31@gmail.com>
2024-11-27 00:08:40 -0300

    Atualizações:
    * Adição da lógica de recebimento dos dados de forma Dinamica após a criação da tarefa

--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -1,88 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import { FlatList, Text, TouchableOpacity, View } from "react-native"
 import { styleList } from "./style";
 import { Input } from "../../components/Input";
 import { MaterialIcons } from "@expo/vector-icons";
 import { Selecionar } from "../../components/Selecionar";
 import { Prioridade } from "../../components/Prioridade";
+import { AuthContextList } from "../../context/authContext_list";
 
 
-type PropCard = {
-    id:number,
-    titulo:string,
-    descricao:string,
-    prioridade:'Urgente' | 'Opcional'
-}
-const data:Array<PropCard> = [
-    {
-        id:0,
-        titulo: "Realizar tarefa de Dados",
-        descricao:"Buscar informações em uma API externa e mesclar os dados",
-        prioridade: "Opcional"
-    },
-    {
-        id:1,
-        titulo: "Construir diagrama de algoritmos ",
-        descricao:"Usar o Drawio para confecionar diagramas ",
-        prioridade: "Urgente"
-    },
-    {
-        id:2,
-        titulo: "Realizar Pull requests no código central",
-        descricao:"Avaliar código e mesclar com a branch principal",
-        prioridade: "Urgente"
-    },
-    {
-        id:3,
-        titulo: "levantar Requisitos dos Softwares",
-        descricao:"Entrevista com os clientes para aferição das necessidades",
-        prioridade: "Urgente"
-    },
-    {
-        id:4,
-        titulo: "Ajuste de Servidor",
-        descricao:"Verificar a logica de requisição das informações",
-        prioridade: "Opcional"
-    },
-    {
-        id:5,
-        titulo: "detalhamento da arquitetura",
-        descricao:"Levantar as infos e definir arquitetura",
-        prioridade: "Urgente"
-    },
-    {
-        id:6,
-        titulo: "Construção do Backend da Aplixação",
-        descricao:"Usar Spring Boot ou Quarkus",
-        prioridade: "Urgente"
-    },
-    {
-        id:7,
-        titulo: "Atualização de Dependencias do projeto",
-        descricao:"Colocar versões atuais",
-        prioridade: "Urgente"
-    },
-    {
-        id:8,
-        titulo: "descrever arquitetura do projeto ",
-        descricao:"Aplicar Microserviços para Operações",
-        prioridade: "Urgente"
-    },
-    {
-        id:9,
-        titulo: "Mantutenção diaria dos servidores",
-        descricao:"ajuste de latencia ",
-        prioridade: "Opcional"
-    },
-    {
-        id:10,
-        titulo: "Verificar a integridade dos dados",
-        descricao:"Aplicar normalização de Banco de Dados",
-        prioridade: "Urgente"
-    }
-]
+
 
 export default function List() {
+    /*Essa const que utiliza o AuthContextType
+    vai recuperar do AuthContextList apenas a taskList
+    que é a variavel da qual deve ser comun aos 2 para pegar os dados criados no 
+    Async Storage e trazer para listagem
+    */
+    const {taskList} = useContext<AuthContextType>(AuthContextList); 
     const card = (item:PropCard) =>{
         return (
             <TouchableOpacity style = {styleList.card}>
@@ -104,7 +38,7 @@ export default function List() {
                             </View>
                     </View>
                     <Prioridade
-                        color="red"
+                        color='red'
                         caption={item.prioridade}
                     />
                 </View>
@@ -121,14 +55,14 @@ export default function List() {
                 <View style = {styleList.viewInput}>
                     <Input
                         IconLeft={MaterialIcons}
-                        IconLeftName="search"
+                        IconLeftName='search'
                     />
                 </View>
             </View>
             <FlatList
-                data={data}
+                data={taskList}
                 style= {styleList.lista}
-                keyExtractor={(item,index) => item.id.toString()}
+                keyExtractor={(item,index) => item.item.toString()}
                 renderItem={({item,index}) => {
                     return (
                         card(item)
