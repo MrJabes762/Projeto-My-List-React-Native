fe1b38efc2331c2528626f7e23f214db7ab14304
Mr Jabes <jabessilva31@gmail.com>
2024-12-06 16:05:07 -0300

    Atualizações:
    *Lógica visual de deslize para deletar (para esquerda) e editar (para direita)

--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -1,11 +1,14 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef } from "react";
 import { FlatList, Text, TouchableOpacity, View } from "react-native"
 import { styleList } from "./style";
 import { Input } from "../../components/Input";
-import { MaterialIcons } from "@expo/vector-icons";
+import { AntDesign, MaterialIcons } from "@expo/vector-icons";
 import { Selecionar } from "../../components/Selecionar";
 import { Prioridade } from "../../components/Prioridade";
 import { AuthContextList } from "../../context/authContext_list";
+import { formatDateToBr } from "../../global/functions";
+import { Directions, Swipeable } from "react-native-gesture-handler";
+import { themes } from "../../global/themes";
 
 
 export default function List() {
@@ -14,43 +17,89 @@ export default function List() {
     que é a variavel da qual deve ser comun aos 2 para pegar os dados criados no 
     Async Storage e trazer para listagem
     */
-    const {taskList} = useContext<AuthContextType>(AuthContextList); 
-    const card = (item:PropCard) =>{
+    const { taskList, handleDelete, handleEditar } = useContext<AuthContextType>(AuthContextList);
+    const swipeableRefs = useRef([]);
+
+    const deletar = () => {
+        return (<View
+            style={styleList.botaoDeletar}
+        >
+            <AntDesign
+                name="delete"
+                size={20}
+                color={'white'}
+            />
+        </View>);
+    }
+    const editar = () => {
+        return (<View
+            style={styleList.botaoEditar}
+        >
+            <AntDesign
+                name="edit"
+                size={20}
+                color={'white'}
+            />
+        </View>);
+    }
+    const handleSwipeableOpem = (directions: 'right' | 'left', item, index) => {
+        if (directions === 'right') {
+            handleDelete(item);
+        } else {
+            handleEditar(item);
+        }
+        swipeableRefs.current[index]?.close();
+    };
+
+    const card = (item: PropCard, index) => {
+
         return (
-            <TouchableOpacity style = {styleList.card}>
-                <View style= {styleList.rowCard}>
-                    <View style = {styleList.rowCardEsqueda}>
-                        <Selecionar/>
+            <Swipeable
+                ref={(ref) => swipeableRefs.current[index] = ref}
+                key={index}
+                renderRightActions={deletar}
+                renderLeftActions={editar}
+                onSwipeableOpen={(directions) => handleSwipeableOpem(directions, item, index)}
+            >
+                <View style={styleList.card}>
+                    <View style={styleList.rowCard}>
+                        <View style={styleList.rowCardEsqueda}>
+                            <Selecionar />
                             <View>
-                                <Text 
-                                    style = {styleList.cardTitulo} 
-                                    numberOfLines= {1} // exibir a info em uma unica linha 
-                                    ellipsizeMode = {'tail'}//para adicionar os 3 pontos 
-                                    >{item.titulo}
+                                <Text
+                                    style={styleList.cardTitulo}
+                                    numberOfLines={1} // exibir a info em uma unica linha 
+                                    ellipsizeMode={'tail'}//para adicionar os 3 pontos 
+                                >{item.titulo}
                                 </Text>
-                                <Text 
-                                    style = {styleList.cardDesc}
-                                    numberOfLines= {1} 
-                                    ellipsizeMode = {'tail'}
-                                    >{item.descricao}</Text>
+                                <Text
+                                    style={styleList.cardDesc}
+                                    numberOfLines={1}
+                                    ellipsizeMode={'tail'}
+                                >{item.descricao}
+                                </Text>
+                                <Text style={styleList.cardData}>
+                                    Até {formatDateToBr(item.tempoLimite)}
+                                </Text>
                             </View>
+                        </View>
+                        <Prioridade
+                            color='red'
+                            caption={item.prioridade}
+                        />
                     </View>
-                    <Prioridade
-                        color='red'
-                        caption={item.prioridade}
-                    />
                 </View>
-            </TouchableOpacity>
+            </Swipeable>
         );
-                        
+
     }
     return (
-        <View style = {styleList.Tela}>
-            <View style = {styleList.cabecalho}>
-                <Text style = {styleList.textoCabecalho}>
+        <View style={styleList.Tela}>
+            <View style={styleList.cabecalho}>
+                <Text style={styleList.textoCabecalho}>
                     Bom dia, Jabes
                 </Text>
-                <View style = {styleList.viewInput}>
+                <View style={styleList.viewInput}>
                     <Input
                         IconLeft={MaterialIcons}
                         IconLeftName='search'
@@ -59,14 +108,14 @@ export default function List() {
             </View>
             <FlatList
                 data={taskList}
-                style= {styleList.lista}
-                keyExtractor={(item,index) => item.item.toString()}
-                renderItem={({item,index}) => {
+                style={styleList.lista}
+                keyExtractor={(item) => item.item.toString()}
+                renderItem={({ item, index }) => {
                     return (
-                        card(item)
+                        card(item, index)
                     );
                 }}
-            />        
+            />
         </View>
     );
 }
